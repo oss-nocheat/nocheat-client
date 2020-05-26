49a6e8db3e52fa7a8c2e96810e6ee562a4830b3a
Myeongwon Choi <auspic7@gmail.com>
2020-05-26 17:51:53 +0900

    formatted file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from "react-router-dom";
-
-import "./assets/styles/app.css";
 
 import Dashboard from "./pages/Dashboard";
 import Exam from "./pages/Exam";
 import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact={true}>
-          <Home></Home>
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard></Dashboard>
-        </Route>
-        <Route path="/exam">
-          <Exam></Exam>
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact={true}>
+                    <Home></Home>
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard></Dashboard>
+                </Route>
+                <Route path="/exam">
+                    <Exam></Exam>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
