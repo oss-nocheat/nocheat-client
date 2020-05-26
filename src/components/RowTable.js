43183ae3e6ef9d853f5cef0a6a52915a6d37790d
Myeongwon Choi <auspic7@gmail.com>
2020-05-26 17:53:27 +0900

    table component

---
+++ src/components/RowTable.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from "react";
+import Table from "reactstrap/es/Table";
+
+export default function RowTable(props) {
+    return props.rows?.length ? (
+        <Table>
+            <thead>
+            <tr>
+                {Object.keys(props.rows[0]).map(key => <th key={key}>{key}</th>)}
+            </tr>
+            </thead>
+            <tbody>
+            {props.rows?.map(row => {
+                return <tr onClick={props.rowClick} id={row.id}>{Object.values(row).map((value => <td key={value}>{value}</td>))}</tr>
+            })}
+            </tbody>
+        </Table>
+    ) : <></>
+}
