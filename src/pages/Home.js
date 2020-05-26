53fefee86b1de2597b72af36ca6fb2a9a0dccd2e
Myeongwon Choi <auspic7@gmail.com>
2020-05-26 17:52:36 +0900

    main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
+import "../assets/styles/app.css";
 import prof_button from "../assets/images/prof_button.png";
 import stu_button from "../assets/images/stu_button.png";
 
 export default function Exam() {
-  return (
-    <>
-      <p id="app-title">NO CHEATING HELPER</p>
-      <p id="choose-mode">choose the mode</p>
-      <div id="grid">
-        <div>
-          <Link to="/dashboard">
-            <img
-              src={prof_button}
-              alt="교수님으로 로그인"
-              width="350"
-              height="350"
-            />
-          </Link>
-        </div>
-        <div>
-          <Link to="/exam">
-            <img
-              src={stu_button}
-              alt="학생으로 로그인"
-              width="350"
-              height="350"
-            />
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <p id="app-title">NO CHEATING HELPER</p>
+            <p id="choose-mode">choose the mode</p>
+            <div id="grid">
+                <div>
+                    <Link to="/dashboard">
+                        <img
+                            src={prof_button}
+                            alt="교수님으로 로그인"
+                            width="350"
+                            height="350"
+                        />
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/exam">
+                        <img
+                            src={stu_button}
+                            alt="학생으로 로그인"
+                            width="350"
+                            height="350"
+                        />
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
